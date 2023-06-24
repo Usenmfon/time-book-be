568077acd35a59f93cf2ed85310c0a5d4b659f63
Usenmfon <usenmfonuko@gmail.com>
2023-06-24 16:22:21 +0100

    update: structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './auth/schema/auth.schema';
+import { AuthModule } from './auth/auth.module';
+import { ServiceExceptionFilter } from './helper/exceptions/filters/service.exception';
+import { Connection } from 'mongoose';
+import { APP_FILTER } from '@nestjs/core';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { SystemModule } from './system/system.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EventEmitterModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      connectionFactory: (connection: Connection) => {
+        return connection;
+      },
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule,
+    SystemModule,
+  ],
+  providers: [{ provide: APP_FILTER, useClass: ServiceExceptionFilter }],
 })
 export class AppModule {}
