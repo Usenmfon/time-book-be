6974a8b25d67efa14baf23b2e4c8bc3ff9f4abc4
Usenmfon <usenmfonuko@gmail.com>
2023-08-16 12:12:11 +0100

    helper method to get start and end days of a week

--- src/helper/main.ts
+++ src/helper/main.ts
@@ -20,3 +20,14 @@ export function clearNullField<T>(obj: T) {
   }
   return obj;
 }
+
+export function getStartAndEndOfWeek(date) {
+  const currentDate = date ? new Date(date) : new Date();
+  const startOfWeek = new Date(currentDate);
+  startOfWeek.setDate(currentDate.getDate() - currentDate.getDay()); // Set to the first day of the week (Sunday)
+
+  const endOfWeek = new Date(currentDate);
+  endOfWeek.setDate(startOfWeek.getDate() + 6); // Set to the last day of the week (Saturday)
+
+  return { startOfWeek, endOfWeek };
+}
